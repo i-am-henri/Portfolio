e03c03b34f103d5537cdbabeb74c17db82c64146
Hen9 <98414850+ProHen9@users.noreply.github.com>
2022-09-16 18:59:50 +0200

    Add files via upload

---
+++ json/projekts.js
@@ -0,0 +1,19 @@
+const jsondb = {
+    projekte: [
+        {
+            name: "Portfolio",
+            nummer: "1",
+            beschreibung: "Mein erstes öffentliches Projekt ist dieses Portfolio. Ich habe es mit Next Js, framer-motion, styles-components und scss umgesetzt.",
+            shortBeschreibung: "Mein erstes Projekt ist diese Website.",
+            url: "portfolio",
+        },
+        {
+            name: "Hello",
+            nummer: "2",
+            beschreibung: "Helllllllo",
+            shortBeschreibung: "moinsen",
+            url: "moinnnn",
+        },
+    ]
+}
+export default jsondb;
